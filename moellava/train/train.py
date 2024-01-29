15bcee805c69853afc3699de0d93cb2935e81f5b
lb203 <62638829+LinB203@users.noreply.github.com>
2024-01-29 20:39:08 +0800

    Update train.py

--- moellava/train/train.py
+++ moellava/train/train.py
@@ -626,7 +626,7 @@ def preprocess_phi(
             if len(parts) != 2:
                 break
             parts[0] += sep
-            # print('after add sep rou, parts', rou, parts)
+            # print('after add sep, parts', parts)
 
             if has_image:
                 round_len = len(tokenizer_image_token(rou, tokenizer)) + 1  # for eos_token
@@ -656,6 +656,104 @@ def preprocess_phi(
         labels=targets,
     )
 
+
+
+def preprocess_openchat(
+    sources,
+    tokenizer: transformers.PreTrainedTokenizer,
+    has_image: bool = False
+) -> Dict:
+    conv = conversation_lib.default_conversation.copy()
+    roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
+
+    # print('00000000000', sources)
+    # Apply prompt templates
+    conversations = []
+    # sys.exit()
+
+    # import ipdb
+    # ipdb.set_trace()
+    for i, source in enumerate(sources):
+        if roles[source[0]["from"]] != conv.roles[0]:
+            # Skip the first one if it is not from human
+            source = source[1:]
+
+        conv.messages = []
+        for j, sentence in enumerate(source):
+            role = roles[sentence["from"]]
+            assert role == conv.roles[j % 2], f"{i}"
+            conv.append_message(role, sentence["value"])
+        conversations.append(conv.get_prompt())
+    # print(11111111, conversations)
+    # Tokenize conversations
+    # print('before tokenizer_image_token', conversations)
+    if has_image:
+        input_ids = torch.stack([tokenizer_image_token(prompt, tokenizer, return_tensors='pt') for prompt in conversations], dim=0)
+        # print(2222222222222, input_ids.shape)
+    else:
+        input_ids = tokenizer(
+            conversations,
+            return_tensors="pt",
+            padding="longest",
+            max_length=tokenizer.model_max_length,
+            truncation=True,
+        ).input_ids
+
+    # print('after tokenizer_image_token input_ids targets', input_ids)
+    targets = input_ids.clone()
+
+    assert conv.sep_style == conversation_lib.SeparatorStyle.TWO
+    # print(tokenizer)
+    # Mask targets
+    sep = conv.sep + conv.roles[1] + ": "
+    # print('sep\n', sep)
+    for conversation, target in zip(conversations, targets):
+        total_len = int(target.ne(tokenizer.pad_token_id).sum())
+        # print('total_len', total_len)
+        rounds = conversation.split(conv.sep2)
+        # print('len(rounds)', len(rounds))
+        cur_len = 1
+        target[:cur_len] = IGNORE_INDEX
+        for i, rou in enumerate(rounds):
+            if rou == "":
+                break
+
+            parts = rou.split(sep)
+            # print('i rou, parts\n', i, rou, parts)
+            if len(parts) != 2:
+                break
+            parts[0] += sep
+            # print('after add sep, parts\n', parts)
+
+            if has_image:
+                round_len = len(tokenizer_image_token(rou, tokenizer))
+                instruction_len = len(tokenizer_image_token(parts[0], tokenizer)) - 2
+            else:
+                round_len = len(tokenizer(rou).input_ids)
+                instruction_len = len(tokenizer(parts[0]).input_ids) - 2
+            # print('instruction_len, target[cur_len : cur_len + instruction_len]\n',
+            #       instruction_len, target[cur_len : cur_len + instruction_len])
+            # print('round_len, target[cur_len : cur_len + round_len]\n',
+            #       round_len, target[cur_len : cur_len + round_len])
+            target[cur_len : cur_len + instruction_len] = IGNORE_INDEX  # instruction_len is before the answer
+
+            cur_len += round_len
+        target[cur_len:] = IGNORE_INDEX
+        # print(cur_len, total_len)
+        if cur_len < tokenizer.model_max_length:
+            # import ipdb
+            # ipdb.set_trace()
+            if cur_len != total_len:
+                target[:] = IGNORE_INDEX
+                print(
+                    f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+                    f" (ignored)"
+                )
+    # print(input_ids, target)
+    return dict(
+        input_ids=input_ids,
+        labels=targets,
+    )
 
 def preprocess_mpt(
     sources,
@@ -769,6 +867,8 @@ def preprocess(
         return preprocess_phi(sources, tokenizer, has_image=has_image)
     if conversation_lib.default_conversation.version.startswith("stablelm"):  # for stablelm
         return preprocess_phi(sources, tokenizer, has_image=has_image)
+    if conversation_lib.default_conversation.version.startswith("openchat"):  # for openchat
+        return preprocess_openchat(sources, tokenizer, has_image=has_image)
     if conversation_lib.default_conversation.version.startswith("v1"):
         return preprocess_v1(sources, tokenizer, has_image=has_image)
     if conversation_lib.default_conversation.version == "mpt":
@@ -943,9 +1043,12 @@ class LazySupervisedDataset(Dataset):
                 data_dict['image'] = image
             elif self.data_args.is_multimodal:
                 # image does not exist in the data, but the model is multimodal
-                crop_size = self.data_args.image_processor.crop_size
-                # crop_size = {'height': 224, 'width': 224}  # dummy image
-                data_dict['image'] = [torch.zeros(3, crop_size['height'], crop_size['width'])]
+                if hasattr(self.data_args.image_processor, 'crop_size'):
+                    crop_size = self.data_args.image_processor.crop_size
+                    data_dict['image'] = [torch.zeros(3, crop_size['height'], crop_size['width'])]
+                else:
+                    size = self.data_args.image_processor.size
+                    data_dict['image'] = [torch.zeros(3, size['height'], size['width'])]
             return data_dict
         except Exception as e:
             print(f'Error with {e}')
