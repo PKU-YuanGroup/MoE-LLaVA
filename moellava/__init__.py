58308319c17f91d349a9f00d34cbcdf2dc3a1ddb
lb203 <62638829+LinB203@users.noreply.github.com>
2024-02-15 17:04:03 +0800

    Update __init__.py

--- moellava/__init__.py
+++ moellava/__init__.py
@@ -8,7 +8,12 @@ if a == '4' and int(b) >= 34:
     from .model import LlavaMistralForCausalLM
     from .model import MoELLaVAMistralForCausalLM
 if a == '4' and int(b) >= 36:
+    from .model import LlavaMiniCPMForCausalLM
+    from .model import MoELLaVAMiniCPMForCausalLM
     from .model import LlavaPhiForCausalLM
     from .model import MoELLaVAPhiForCausalLM
     from .model import LlavaStablelmForCausalLM
     from .model import MoELLaVAStablelmForCausalLM
+if a == '4' and int(b) >= 37:
+    from .model import LlavaQwen1_5ForCausalLM
+    from .model import MoELLaVAQwen1_5ForCausalLM
