a231ab5d9010f9d66b6eb0ee7bfe1268c9b548f8
lb203 <62638829+LinB203@users.noreply.github.com>
2024-02-15 19:09:56 +0800

    Create llava_minicpm.py

---
+++ moellava/model/language_model/llava_minicpm.py
@@ -0,0 +1,135 @@
+#    Copyright 2023 Haotian Liu
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+
+from typing import List, Optional, Tuple, Union
+
+import torch
+import torch.nn as nn
+
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+from .minicpm.configuration_minicpm import MiniCPMConfig
+from .minicpm.modeling_minicpm import MiniCPMModel, MiniCPMForCausalLM
+
+from transformers.modeling_outputs import CausalLMOutputWithPast
+
+from ..llava_arch import LlavaMetaModel, LlavaMetaForCausalLM
+import torch.distributed as dist
+
+
+class LlavaMiniCPMConfig(MiniCPMConfig):
+    model_type = "llava_minicpm"
+
+
+class LlavaMiniCPMModel(LlavaMetaModel, MiniCPMModel):
+    config_class = LlavaMiniCPMConfig
+
+    def __init__(self, config: MiniCPMConfig):
+        super(LlavaMiniCPMModel, self).__init__(config)
+
+
+class LlavaMiniCPMForCausalLM(MiniCPMForCausalLM, LlavaMetaForCausalLM):
+    config_class = LlavaMiniCPMConfig
+
+    def __init__(self, config):
+        super(MiniCPMForCausalLM, self).__init__(config)
+        self.model = LlavaMiniCPMModel(config)
+        self.vocab_size = config.vocab_size
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def get_model(self):
+        return self.model
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        images: Optional[torch.FloatTensor] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+
+        # import ipdb
+        # ipdb.set_trace()
+        # print(f'rank {dist.get_rank()}', 'before prepare_inputs_labels_for_multimodal')
+        if inputs_embeds is None:
+            (
+                input_ids,
+                position_ids,
+                attention_mask,
+                past_key_values,
+                inputs_embeds,
+                labels
+            ) = self.prepare_inputs_labels_for_multimodal(
+                input_ids,
+                position_ids,
+                attention_mask,
+                past_key_values,
+                labels,
+                images
+            )
+
+        # dist.barrier()
+        # print(f'rank {dist.get_rank()}', 'after prepare_inputs_labels_for_multimodal')
+        out = super().forward(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            labels=labels,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict
+        )
+        # dist.barrier()
+        # print(f'rank {dist.get_rank()}', 'after LLM')
+        return out
+
+
+    def prepare_inputs_for_generation(
+            self, input_ids, past_key_values=None, attention_mask=None, inputs_embeds=None, **kwargs
+    ):
+        if past_key_values:
+            input_ids = input_ids[:, -1:]
+
+        # if `inputs_embeds` are passed, we only want to use them in the 1st generation step
+        if inputs_embeds is not None and past_key_values is None:
+            model_inputs = {"inputs_embeds": inputs_embeds}
+        else:
+            model_inputs = {"input_ids": input_ids}
+
+        model_inputs.update(
+            {
+                "past_key_values": past_key_values,
+                "use_cache": kwargs.get("use_cache"),
+                "attention_mask": attention_mask,
+                "images": kwargs.get("images", None),
+            }
+        )
+        return model_inputs
+
+AutoConfig.register("llava_minicpm", LlavaMiniCPMConfig)
+# AutoTokenizer.register(LlavaPhiConfig, PhiTokenizer)
+AutoModelForCausalLM.register(LlavaMiniCPMConfig, LlavaMiniCPMForCausalLM)
